59f1a3a712e94fa52faef7bfb80791f3b7d1e2af
mosstea <mosstea@localhost>
2017-05-29 18:56:04 -0400

    added linked list

---
+++ List/IntList.java
@@ -0,0 +1,96 @@
+public class IntList {
+
+	int first;
+	IntList rest;
+
+	public IntList(int f, IntList r) {
+		first = f;
+		rest = r;
+	}
+
+	/** Return the size of list recursively. */
+	public int size() {
+		if (rest == null) {
+			return 1;
+		} else {
+			return 1 + this.rest.size();
+		}
+	}
+
+	/** Return the size of list iteratively. */
+	public int size2() {
+		IntList cur = this;
+		int size = 0;
+		while (cur != null) {
+			size += 1;
+			cur = cur.rest;
+		}
+		return size;
+	}
+
+	/** Return the ith item in the list recursively. */
+	public int get(int i) {
+		if (i == 0) {
+			return first;
+		} else {
+			return this.rest.get(i - 1);
+		}
+	}
+
+	/** Return the ith item in the list iteratively. */
+	public int get2(int i) {
+		IntList cur = this;
+		while (i > 0) {
+			cur = cur.rest;
+			i -= 1;
+		}
+		return cur.first;		
+	}
+
+	/** Increase all the elements in this list by x. */
+	public static IntList incrList(IntList l, int x) {
+		IntList q = new IntList(l.first + x, null);
+		IntList curq = q;
+		IntList cur = l.rest;
+		while (cur != null) {
+			System.out.println("cur.first" + cur.first);
+			curq.rest = new IntList(cur.first + x, null);
+			curq = curq.rest;
+			cur = cur.rest;
+		}
+		return q;
+	}
+
+	/** Increase all the elements in this list by x. */
+	public static IntList dincrList(IntList l, int x) {
+		IntList cur = l;
+		while (cur != null) {
+			cur.first += x;
+			cur = cur.rest;
+		}
+		return l;
+	}
+
+
+	public static void main(String[] args) {
+		IntList l = new IntList(15, null);
+		l = new IntList(10, l);
+		l = new IntList(5, l);
+		System.out.println("size exercise");
+		System.out.println(l.size());
+		System.out.println(l.size2());
+		System.out.println("get exercise");
+		System.out.println(l.get(1));
+		System.out.println(l.get2(1));
+		System.out.println("increase all elements in the list exercise copied");
+		IntList q = incrList(l, 5);
+		System.out.println(q.first);
+		System.out.println(q.rest.first);
+		System.out.println(q.rest.rest.first);
+		System.out.println("increase all elements in the list exercise onself");
+		System.out.println(dincrList(l, 5).first);
+		System.out.println(l.rest.first);
+		System.out.println(l.rest.rest.first);
+
+	}
+}
---
+++ List/SLList.java
@@ -0,0 +1,70 @@
+public class SLList {
+
+	private class IntNode {
+		public int item;
+		public IntNode next;
+
+		public IntNode(int i, IntNode n) {
+			item = i;
+			next = n;
+		}	
+	}
+
+	public IntNode first;
+
+	public SLList(int x) {
+		first = new IntNode(x, null);
+	}
+
+	public void addFirst(int x) {
+		first = new IntNode(x, first);
+	}
+
+	public int getFirst() {
+		return first.item;
+	}
+
+	public void addLast(int x) {
+		IntNode cur = first;
+		while (cur.next != null) {
+			cur = cur.next;
+		}
+		cur.next = new IntNode(x, null);
+	}
+
+	public int size() {
+		int size = 0;
+		IntNode cur = first;
+		while (cur != null) {
+			size += 1;
+			cur = cur.next;
+		}
+		return size;
+	}
+
+	/** Returns the size of list starts from IntNode p. */
+	private static int size_helper(IntNode p) {
+		if (p.next == null) {
+			return 1;
+		} else {
+			return 1 + size_helper(p.next);
+		}
+	}
+
+	/** Calculate size recursively? no, not everyone has a first? or we need to copy the rest to a new SSList? */
+	public int size2() {
+		return size_helper(first);
+	}
+
+	public static void main(String[] args) {
+		SLList s = new SLList(10);
+		System.out.println(s.getFirst());
+		s.addFirst(120);
+		System.out.println(s.getFirst());
+		System.out.println(s.size());
+		s.addLast(5);
+		System.out.println(s.size());
+		System.out.println(s.size2());
+
+	}
+}
