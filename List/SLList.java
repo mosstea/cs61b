3c0873bfd3df4df7570f3ac38e31c43da5e07a3d
mosstea <mosstea@localhost>
2017-05-30 14:08:31 -0400

    implemented simple genetics

--- List/SLList.java
+++ List/SLList.java
@@ -1,61 +1,50 @@
-public class SLList {
+public class SLList<type>{
 
-	private class IntNode {
-		public int item;
-		public IntNode next;
+	private class Node {
+		public type item;
+		public Node next;
 
-		public IntNode(int i, IntNode n) {
+		public Node(type i, Node n) {
 			item = i;
 			next = n;
 		}	
 	}
 
 	/** The first element in the list should be sentinel.next */
-	private IntNode sentinel;
+	private Node first;
 	public int size; 	// 1. idea: caching
 						// 2. if size is made private, add a simple public method that can return size
-
-	/** Represents an empty list. */
-	public SLList() {
-		sentinel = new IntNode(0, null);
-		size = 0;
-	}
 	
-	public SLList(int x) {
-		sentinel = new IntNode(0, null);
-		sentinel.next = new IntNode(x, null);
+	public SLList(type x) {
+		first = new Node(x, null);
 		size = 1;
 	}
 
-	public void addFirst(int x) {
-		sentinel.next = new IntNode(x, sentinel.next);
+	public void addFirst(type x) {
+		first = new Node(x, first);
 		size += 1;
 	}
 
-	public int getFirst() {
-		if (size != 0) {
-			return sentinel.next.item;
-		} else {
-			return -10000;	// however, there is still special case here
-		}
+	public type getFirst() {
+		return first.item;
 	}
 
-	public void addLast(int x) {
-		IntNode cur = sentinel;
+	public void addLast(type x) {
+		Node cur = first;
 		while (cur.next != null) {
 			cur = cur.next;
 		}
-		cur.next = new IntNode(x, null);
+		cur.next = new Node(x, null);
 		size += 1;
 	}
 
 	public static void main(String[] args) {
-		SLList s = new SLList();
+		SLList<String> s = new SLList<String>("apple");
 		System.out.println(s.getFirst());
-		s.addFirst(120);
+		s.addFirst("lemon");
 		System.out.println(s.getFirst());
 		System.out.println(s.size);
-		s.addLast(5);
+		s.addLast("grape");
 		System.out.println(s.size);
 	}
 }
