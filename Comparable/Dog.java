cec8dad38360e140f4e95d926b8c12d2a2728086
mosstea <mosstea@localhost>
2017-06-12 12:16:55 -0400

    added generics to avoid ugly casting from Object

--- Comparable/Dog.java
+++ Comparable/Dog.java
@@ -1,4 +1,4 @@
-public class Dog implements OurComparable {
+public class Dog implements OurComparable<Dog> {
 	private String name;
 	public int size;
 
@@ -8,12 +8,11 @@ public class Dog implements OurComparable {
 	}
 
 	public void bark() {
-		System.out.println(name + " is barking");
+		System.out.println(name + " is barking!");
 	}
 
 	@Override
-	public int compareTo(Object o) {
-		Dog dog = (Dog) o;
+	public int compareTo(Dog dog) {
 		return size - dog.size;
 	}
 }
--- Comparable/OurComparable.java
+++ Comparable/OurComparable.java
@@ -1,7 +1,7 @@
-public interface OurComparable {
+public interface OurComparable<Type> {
 	/* Return negative if this object is smaller
 	 * Return 0 if equals
 	 * Return positive if this object is larger
 	 */
-	int compareTo(Object o);
+	int compareTo(Type o);
 }
